74c7153b7bb706899c03c5fda12f88985e1e41af
minhtien <124766467+minhtien2002@users.noreply.github.com>
2024-06-14 18:15:56 +0700

    tesst

--- src/App.tsx
+++ src/App.tsx
@@ -7,29 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save ghfhgfghhghgj
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
         <div>
           <h1>hello</h1>
         </div>
