fefea51f8cfa0e9a3767bec6c83c2ceeb9daf040
minhtien <124766467+minhtien2002@users.noreply.github.com>
2024-06-16 01:23:51 +0700

    config env (port,...)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({mode}) => {
+
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    server: {
+      port: parseInt(env.PORT),
+    },
+  }
+
 })
